d86200fa17acaf588543cbc95c40ab24b69679b6
VikVanDik <95vlc17@gmail.com>
2023-09-29 14:50:12 +0200

    [add] : data

--- script/script.js
+++ script/script.js
@@ -1,1 +1,29 @@
 // 2. Creare i data che mi serviranno a gestire le azioni in pagina.
+
+const { createApp } = Vue;
+
+createApp ({
+  data () {
+    return {
+      tasks : [
+        {
+          text : 'fare la spesa',
+          flagDone : false
+        },
+        {
+          text : 'pagare la bolletta',
+          flagDone : false
+        }
+      ],
+      tasksDone : [],
+    }
+  },
+
+  methods : {
+
+  },
+  
+  mounted () {
+    
+  }
+})
